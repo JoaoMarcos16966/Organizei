801b2fe85c1bd5831ae6faaaedc21b7d97a007b7
João Marcos <joaomarcospndl88@gmail.com>
2026-06-25 09:51:55 -0300

    Initial Commit

---
+++ Organizei/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Organizei
+//
+//  Created by João Marcos Pimentel Nascimento de Lima  on 25/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Organizei/OrganizeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizeiApp.swift
+//  Organizei
+//
+//  Created by João Marcos Pimentel Nascimento de Lima  on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct OrganizeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
