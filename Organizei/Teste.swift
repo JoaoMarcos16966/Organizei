6b16eb1ae22e0813cbec8270e8992190fb3a1924
joao moraes <jmjoaommoraes@gmail.com>
2026-06-25 10:49:18 -0300

    first commit

--- Organizei/Teste.swift
+++ Organizei/Teste.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct Teste: View {
+struct Teste1: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Teste()
+    Teste1()
 }
