5957919179e442d68b6f22a031244e7e5b1dd231
João Marcos <joaomarcospndl88@gmail.com>
2026-06-25 10:15:27 -0300

    Adicionei classe teste na pasta testa

---
+++ Organizei/Teste.swift
@@ -0,0 +1,18 @@
+//
+//  Teste.swift
+//  Organizei
+//
+//  Created by João Marcos Pimentel Nascimento de Lima  on 25/06/26.
+//
+
+import SwiftUI
+
+struct Teste: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Teste()
+}
