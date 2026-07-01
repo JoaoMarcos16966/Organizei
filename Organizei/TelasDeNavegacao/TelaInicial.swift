ff579292422ff016fc10f648e744417558b13ab3
joao moraes <jvmmoraesdev@gmail.com>
2026-07-01 18:36:59 -0300

    Adicionado o dia para a navigationTitle conforme o dia selecionado

--- Organizei/TelasDeNavegacao/TelaInicial.swift
+++ Organizei/TelasDeNavegacao/TelaInicial.swift
@@ -10,20 +10,22 @@ import SwiftUI
 
 struct TelaInicial: View {
     @State var selectedDay: Date = .now
-    @State var diaDeHoje: Date = .now
+    //    @State var diaDeHoje: Date = .now
 
     var body: some View {
-        @State var TituloDaNavegacao: String = Calendar.current.isDate(diaDeHoje, inSameDayAs: selectedDay) ? "Hoje" : "Nao hoje"
-        
+        @State var TituloDaNavegacao: String =
+            Calendar.current.isDate(Date.now, inSameDayAs: selectedDay)
+        ? "Hoje" : "\(selectedDay.formatted(.dateTime.day().month(.wide)))"
+
         NavigationStack {
 
             VStack {
                 HDatePicker(selectedDay: $selectedDay)
-                Text(selectedDay.description)
-                
-            Spacer()
-                
+
+                Spacer()
+
                     .navigationTitle(TituloDaNavegacao)  // Trocar pelo dia selecionado, se dataEscolhida == date.now(), navigationTitle = Hoje
+
                     .toolbar {
                         ToolbarItem(placement: .topBarTrailing) {
                             NavigationLink {
